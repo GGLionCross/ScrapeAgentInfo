c7f4b299bb23a41275f969c69ce421fe9134dbcf
Lionel Quiambao <lionelquiambao@gmail.com>
2024-01-04 16:44:07 -0800

    Renamed make.bat to run.bat

--- run.bat
+++ run.bat
@@ -3,16 +3,13 @@
 REM make.bat is designed to replicated make functionality or npm scripts
 
 REM Local Workplace Commands
-if %1 EQU run py scrape-agent-info.py
-
-REM Utility Commands
-if %1 EQU show-clean-path ECHO %PATH:;=&echo.%
+if %1 EQU app .env\Scripts\python.exe main.py
 
 REM Remind user of available functions
 IF %1 EQU --help (
   ECHO --------------
   ECHO AVAILABLE ARGS
   ECHO --------------
-  ECHO run
+  ECHO app
   ECHO --------------
 )
